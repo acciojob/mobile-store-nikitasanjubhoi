b22f9c5ede6cbdce519cf577e66ef3d86d964147
Nikita bhoi <114177037+nikitasanjubhoi@users.noreply.github.com>
2025-11-04 11:57:36 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,1 +1,14 @@
 
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <Link to="/">Home</Link>
+      <Link to="/admin">Admin</Link>
+    </header>
+  );
+};
+
+export default Header;
